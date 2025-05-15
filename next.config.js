99fea5aca9a8f725dd054238fa9e24a1ee5f6b52
Dom <72518625+DLPDigital@users.noreply.github.com>
2025-05-15 18:29:14 +0100

    Add Netlify configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  // Ensure no trailing slash is added to URLs
+  trailingSlash: false,
 }
 
 module.exports = nextConfig
