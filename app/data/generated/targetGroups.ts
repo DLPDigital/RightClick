12e00824611cb31d4b8bd8e7b8cb2c2a52bed782
Dom <72518625+DLPDigital@users.noreply.github.com>
2025-06-05 15:20:53 +0100

    stop tracking generated data

--- app/data/generated/targetGroups.ts
+++
@@ -1,8 +0,0 @@
-import { TargetCategoryType, VerbCategoryType } from "../../types"
-
-export const verbTargetCompatibility: Record<VerbCategoryType, TargetCategoryType[]> = {
-  destruction: ["locationGood", "thingGood", "abstractGood", "mediaGood"],
-  control: ["locationGood", "thingGood", "abstractGood"],
-  creation: ["locationBad", "thingBad", "abstractBad"],
-  acquisition: ["thingBad", "abstractBad", "mediaGood"],
-}
