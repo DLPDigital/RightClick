f9fbbc40c0e8e98a1ade7de7d37d0c98b8b49054
Dom <72518625+DLPDigital@users.noreply.github.com>
2025-06-12 23:18:00 +0100

    add format number

--- app/components/Achievements/Achievements.tsx
+++ app/components/Achievements/Achievements.tsx
@@ -3,6 +3,7 @@ import { Achievement, InsanityStage } from "../../types"
 import { AchievementItem } from "./Item"
 
 import { achievementsScreen, achievementList } from "./Achievements.css"
+import { formatNumber } from "../../utils/formatters"
 
 interface AchievementsScreenProps {
   allAchievements: Record<string, Achievement>
@@ -21,7 +22,7 @@ export const Achievements: React.FC<AchievementsScreenProps> = ({
     <div className={achievementsScreen}>
       <h2>Clout & Achievements</h2>
       <p>
-        You have posted {postsMade} times! Your audience are rated &ldquo;{insanity.name}&rdquo;.
+        You have posted {formatNumber(postsMade)} times! Your audience are rated &ldquo;{insanity.name}&rdquo;.
       </p>
       <p>Many are thinking &ldquo;{insanity.description}&rdquo;.</p>
       <h3>Achievements</h3>
