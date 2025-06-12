1ef69e74bb708436584f821238a41edd40c8e3a7
Dom <72518625+DLPDigital@users.noreply.github.com>
2025-06-12 22:28:51 +0100

    feat: installing lodash to add debounce to post button

--- app/utils/__tests__/buildSentence.test.ts
+++ app/utils/__tests__/buildSentence.test.ts
@@ -11,17 +11,27 @@ describe("buildSentence", () => {
   }
 
   it("returns a sentence with correct verb for singular subject", () => {
-    const result = buildSentence((subjectSingular as PostSubject), "controlling", "the weather", engagements)
+    const result = buildSentence(
+      subjectSingular as PostSubject,
+      "controlling",
+      "the weather",
+      engagements
+    )
     expect(result.content).toMatch(/^The government is controlling the weather\./)
   })
 
   it("returns a sentence with correct verb for plural subject", () => {
-    const result = buildSentence((subjectPlural as PostSubject), "invading", "Earth", engagements)
+    const result = buildSentence(subjectPlural as PostSubject, "invading", "Earth", engagements)
     expect(result.content).toMatch(/^Aliens are invading Earth\./)
   })
 
   it("generates between 3 and 8 hashtags, each prefixed with #", () => {
-    const result = buildSentence((subjectSingular as PostSubject), "testing", "something", engagements)
+    const result = buildSentence(
+      subjectSingular as PostSubject,
+      "testing",
+      "something",
+      engagements
+    )
     const hashtags = result.hashtags.split(" ")
     expect(hashtags.length).toBeLessThan(10)
     expect(hashtags.length).toBeGreaterThan(2)
